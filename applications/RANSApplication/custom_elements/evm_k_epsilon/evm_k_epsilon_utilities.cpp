99644780f50d67046916d89ae2fc1b4dd44ca293
sunethwarna <suneth.warna@gmail.com>
2019-11-19 14:14:05 +0100

    minor

--- applications/RANSApplication/custom_elements/evm_k_epsilon/evm_k_epsilon_utilities.cpp
+++ applications/RANSApplication/custom_elements/evm_k_epsilon/evm_k_epsilon_utilities.cpp
@@ -86,9 +86,7 @@ double CalculateGamma(const double C_mu,
                       const double turbulent_kinematic_viscosity)
 {
     return RansCalculationUtilities::SoftPositive(
-        C_mu * f_mu * turbulent_kinetic_energy /
-        RansCalculationUtilities::SoftMax(turbulent_kinematic_viscosity,
-                                          std::numeric_limits<double>::epsilon()));
+        C_mu * f_mu * turbulent_kinetic_energy / turbulent_kinematic_viscosity);
 }
 
 template double CalculateSourceTerm<2>(const BoundedMatrix<double, 2, 2>&, const double, const double);
