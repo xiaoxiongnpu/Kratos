e9dcbdb443b4205b7f0b75339e7cb1c7e91c7962
Philipp <philipp.bucher@tum.de>
2019-12-16 12:13:18 +0100

    adding empty MassElement

---
+++ applications/StructuralMechanicsApplication/custom_elements/mass_element.cpp
@@ -0,0 +1,512 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+
+// System includes
+
+
+// External includes
+
+
+// Include Base h
+#include "mass_element.h"
+
+
+namespace Kratos
+{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * Constructor.
+ */
+MassElement::MassElement(IndexType NewId)
+    : Element(NewId)
+{
+}
+
+/**
+ * Constructor using an array of nodes
+ */
+MassElement::MassElement(IndexType NewId, const NodesArrayType& ThisNodes)
+    : Element(NewId, ThisNodes)
+{
+}
+
+/**
+ * Constructor using Geometry
+ */
+MassElement::MassElement(IndexType NewId, GeometryType::Pointer pGeometry)
+    : Element(NewId, pGeometry)
+{
+}
+
+/**
+ * Constructor using Properties
+ */
+MassElement::MassElement(IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties)
+    : Element(NewId, pGeometry, pProperties)
+{
+}
+
+/**
+ * Copy Constructor
+ */
+MassElement::MassElement(MassElement const& rOther)
+    : Element(rOther)
+{
+}
+
+/**
+ * Destructor
+ */
+MassElement::~MassElement()
+{
+}
+
+///@}
+///@name Operators
+///@{
+
+/// Assignment operator.
+MassElement & MassElement::operator=(MassElement const& rOther)
+{
+    BaseType::operator=(rOther);
+    Flags::operator =(rOther);
+    // mpProperties = rOther.mpProperties;
+    return *this;
+}
+
+///@}
+///@name Operations
+///@{
+
+/**
+ * ELEMENTS inherited from this class have to implement next
+ * Create and Clone methods: MANDATORY
+ */
+
+/**
+ * creates a new element pointer
+ * @param NewId: the ID of the new element
+ * @param ThisNodes: the nodes of the new element
+ * @param pProperties: the properties assigned to the new element
+ * @return a Pointer to the new element
+ */
+Element::Pointer MassElement::Create(
+    IndexType NewId,
+    NodesArrayType const& ThisNodes,
+    PropertiesType::Pointer pProperties) const
+{
+    KRATOS_TRY
+    return Kratos::make_intrusive<MassElement>(NewId, GetGeometry().Create(ThisNodes), pProperties);
+    KRATOS_CATCH("");
+}
+
+/**
+ * creates a new element pointer
+ * @param NewId: the ID of the new element
+ * @param pGeom: the geometry to be employed
+ * @param pProperties: the properties assigned to the new element
+ * @return a Pointer to the new element
+ */
+Element::Pointer MassElement::Create(
+    IndexType NewId,
+    GeometryType::Pointer pGeom,
+    PropertiesType::Pointer pProperties) const
+{
+    KRATOS_TRY
+    return Kratos::make_intrusive<MassElement>(NewId, pGeom, pProperties);
+    KRATOS_CATCH("");
+}
+
+/**
+ * creates a new element pointer and clones the previous element data
+ * @param NewId: the ID of the new element
+ * @param ThisNodes: the nodes of the new element
+ * @param pProperties: the properties assigned to the new element
+ * @return a Pointer to the new element
+ */
+Element::Pointer MassElement::Clone(IndexType NewId, NodesArrayType const& ThisNodes) const
+{
+    KRATOS_TRY
+    return Kratos::make_intrusive<MassElement>(NewId, GetGeometry().Create(ThisNodes), pGetProperties());
+    KRATOS_CATCH("");
+}
+
+/**
+ * this determines the elemental equation ID vector for all elemental
+ * DOFs
+ * @param rResult: the elemental equation ID vector
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::EquationIdVector(EquationIdVectorType& rResult, ProcessInfo& CurrentProcessInfo)
+{
+    unsigned int number_of_nodes = GetGeometry().PointsNumber();
+    if (rResult.size() != number_of_nodes)
+        rResult.resize(number_of_nodes, false);
+
+}
+
+/**
+ * determines the elemental list of DOFs
+ * @param ElementalDofList: the list of DOFs
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::GetDofList(DofsVectorType& rElementalDofList, ProcessInfo& CurrentProcessInfo)
+{
+    unsigned int number_of_nodes = GetGeometry().PointsNumber();
+    if (rElementalDofList.size() != number_of_nodes)
+        rElementalDofList.resize(number_of_nodes);
+
+}
+
+/**
+ * ELEMENTS inherited from this class have to implement next
+ * CalculateLocalSystem, CalculateLeftHandSide and CalculateRightHandSide methods
+ * they can be managed internally with a private method to do the same calculations
+ * only once: MANDATORY
+ */
+
+/**
+ * this is called during the assembling process in order
+ * to calculate all elemental contributions to the global system
+ * matrix and the right hand side
+ * @param rLeftHandSideMatrix: the elemental left hand side matrix
+ * @param rRightHandSideVector: the elemental right hand side
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateLocalSystem(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ProcessInfo& rCurrentProcessInfo)
+{
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the elemental left hand side matrix only
+ * @param rLeftHandSideMatrix: the elemental left hand side matrix
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateLeftHandSide(MatrixType& rLeftHandSideMatrix, ProcessInfo& rCurrentProcessInfo)
+{
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the elemental right hand side vector only
+ * @param rRightHandSideVector: the elemental right hand side vector
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateRightHandSide(VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo)
+{
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the first derivatives contributions for the LHS and RHS
+ * @param rLeftHandSideMatrix: the elemental left hand side matrix
+ * @param rRightHandSideVector: the elemental right hand side
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateFirstDerivativesContributions(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ProcessInfo& rCurrentProcessInfo)
+{
+    if (rLeftHandSideMatrix.size1() != 0)
+        rLeftHandSideMatrix.resize(0, 0, false);
+    if (rRightHandSideVector.size() != 0)
+        rRightHandSideVector.resize(0, false);
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the elemental left hand side matrix for the first derivatives constributions
+ * @param rLeftHandSideMatrix: the elemental left hand side matrix
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateFirstDerivativesLHS(MatrixType& rLeftHandSideMatrix, ProcessInfo& rCurrentProcessInfo)
+{
+    if (rLeftHandSideMatrix.size1() != 0)
+        rLeftHandSideMatrix.resize(0, 0, false);
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the elemental right hand side vector for the first derivatives constributions
+ * @param rRightHandSideVector: the elemental right hand side vector
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateFirstDerivativesRHS(VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo)
+{
+    if (rRightHandSideVector.size() != 0)
+        rRightHandSideVector.resize(0, false);
+}
+
+/**
+ * ELEMENTS inherited from this class must implement this methods
+ * if they need to add dynamic element contributions
+ * note: second derivatives means the accelerations if the displacements are the dof of the analysis
+ * note: time integration parameters must be set in the rCurrentProcessInfo before calling these methods
+ * CalculateSecondDerivativesContributions,
+ * CalculateSecondDerivativesLHS, CalculateSecondDerivativesRHS methods are : OPTIONAL
+ */
+
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the second derivative contributions for the LHS and RHS
+ * @param rLeftHandSideMatrix: the elemental left hand side matrix
+ * @param rRightHandSideVector: the elemental right hand side
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateSecondDerivativesContributions(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ProcessInfo& rCurrentProcessInfo)
+{
+    if (rLeftHandSideMatrix.size1() != 0)
+        rLeftHandSideMatrix.resize(0, 0, false);
+    if (rRightHandSideVector.size() != 0)
+        rRightHandSideVector.resize(0, false);
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the elemental left hand side matrix for the second derivatives constributions
+ * @param rLeftHandSideMatrix: the elemental left hand side matrix
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateSecondDerivativesLHS(
+    MatrixType& rLeftHandSideMatrix,
+    ProcessInfo& rCurrentProcessInfo)
+{
+    if (rLeftHandSideMatrix.size1() != 0)
+        rLeftHandSideMatrix.resize(0, 0, false);
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the elemental right hand side vector for the second derivatives constributions
+ * @param rRightHandSideVector: the elemental right hand side vector
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateSecondDerivativesRHS(
+    VectorType& rRightHandSideVector,
+    ProcessInfo& rCurrentProcessInfo)
+{
+    if (rRightHandSideVector.size() != 0)
+        rRightHandSideVector.resize(0, false);
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the elemental mass matrix
+ * @param rMassMatrix: the elemental mass matrix
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateMassMatrix(MatrixType& rMassMatrix, ProcessInfo& rCurrentProcessInfo)
+{
+    if (rMassMatrix.size1() != 0)
+        rMassMatrix.resize(0, 0, false);
+}
+
+/**
+ * this is called during the assembling process in order
+ * to calculate the elemental damping matrix
+ * @param rDampingMatrix: the elemental damping matrix
+ * @param rCurrentProcessInfo: the current process info instance
+ */
+void MassElement::CalculateDampingMatrix(MatrixType& rDampingMatrix, ProcessInfo& rCurrentProcessInfo)
+{
+    if (rDampingMatrix.size1() != 0)
+        rDampingMatrix.resize(0, 0, false);
+}
+
+/**
+ * This method provides the place to perform checks on the completeness of the input
+ * and the compatibility with the problem options as well as the contitutive laws selected
+ * It is designed to be called only once (or anyway, not often) typically at the beginning
+ * of the calculations, so to verify that nothing is missing from the input
+ * or that no common error is found.
+ * @param rCurrentProcessInfo
+ * this method is: MANDATORY
+ */
+int MassElement::Check(const ProcessInfo& rCurrentProcessInfo)
+{
+    KRATOS_TRY
+
+    KRATOS_ERROR_IF(this->Id() < 1) <<"MassElement found with Id 0 or negative" << std::endl;
+
+    KRATOS_ERROR_IF(this->GetGeometry().Area() <= 0) << "On MassElement -> "
+        << this->Id() <<  "; Area cannot be less than or equal to 0" << std::endl;
+
+    return 0;
+
+    KRATOS_CATCH("");
+}
+
+///@}
+///@name Access
+///@{
+
+
+///@}
+///@name Inquiry
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+/// Turn back information as a string.
+
+std::string MassElement::Info() const {
+    std::stringstream buffer;
+    buffer << "MassElement #" << Id();
+    return buffer.str();
+}
+
+/// Print information about this object.
+
+void MassElement::PrintInfo(std::ostream& rOStream) const {
+    rOStream << "MassElement #" << Id();
+}
+
+/// Print object's data.
+
+void MassElement::PrintData(std::ostream& rOStream) const {
+    pGetGeometry()->PrintData(rOStream);
+}
+
+///@}
+///@name Friends
+///@{
+
+///@}
+
+///@name Protected static Member Variables
+///@{
+
+///@}
+///@name Protected member Variables
+///@{
+
+///@}
+///@name Protected Operators
+///@{
+
+///@}
+///@name Protected Operations
+///@{
+
+///@}
+///@name Protected  Access
+///@{
+
+///@}
+///@name Protected Inquiry
+///@{
+
+///@}
+///@name Protected LifeCycle
+///@{
+
+///@}
+
+///@name Static Member Variables
+///@{
+
+///@}
+///@name Member Variables
+///@{
+
+///@}
+///@name Private Operators
+///@{
+
+///@}
+///@name Private Operations
+///@{
+
+///@}
+///@name Serialization
+///@{
+
+void MassElement::save(Serializer& rSerializer) const
+{
+    KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Element );
+
+    // List
+    // To be completed with the class member list
+}
+
+void MassElement::load(Serializer& rSerializer)
+{
+    KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, Element );
+
+    // List
+    // To be completed with the class member list
+}
+
+///@}
+///@name Private  Access
+///@{
+
+///@}
+///@name Private Inquiry
+///@{
+
+///@}
+///@name Un accessible methods
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name Input and output
+///@{
+
+/// input stream function
+inline std::istream & operator >> (std::istream& rIStream, MassElement& rThis);
+
+/// output stream function
+inline std::ostream & operator << (std::ostream& rOStream, const MassElement& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << " : " << std::endl;
+    rThis.PrintData(rOStream);
+    return rOStream;
+}
+
+} // namespace Kratos.
