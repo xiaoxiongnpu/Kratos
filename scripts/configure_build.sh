067de6c5a4e8ff2e3c56bf8ad4f0e69ee1864611
Joaquín <jgonzalez@cimne.upc.edu>
2020-01-21 12:40:10 +0100

    Update to the new compilation format

---
+++ scripts/configure_build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Please do not modify this script
+
+# You can use your interpreter of choice (bash, sh, zsh, ...)
+
+# For any question please contact with us in:
+#   - https://github.com/KratosMultiphysics/Kratos
+
+# Optional parameters:
+# You can find a list will all the compiation options in INSTALL.md or here:
+#   - https://github.com/KratosMultiphysics/Kratos/wiki/Compilation-options
+
+# Function to add apps
+add_app () {
+    export KRATOS_APPLICATIONS="${KRATOS_APPLICATIONS}$1;"
+}
+
+# Set compiler
+export CC=gcc
+export CXX=g++
+
+# Set variables
+export KRATOS_SOURCE="${KRATOS_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"/..}"
+export KRATOS_BUILD="${KRATOS_SOURCE}/build"
+export KRATOS_APP_DIR="${KRATOS_SOURCE}/applications"
+export KRATOS_INSTALL_PYTHON_USING_LINKS=ON
+
+# Set basic configuration
+export KRATOS_BUILD_TYPE=${KRATOS_BUILD_TYPE:-"RelWithDebInfo"}
+export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"/usr/bin/python3"}
+export BOOST_ROOT=${BOOST_ROOT:-"/home/jgonzalez/boost_1_72_0"}
+
+# Set applications to compile
+export KRATOS_APPLICATIONS=
+add_app ${KRATOS_APP_DIR}/ExternalSolversApplication
+add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
+add_app ${KRATOS_APP_DIR}/PfemApplication
+add_app ${KRATOS_APP_DIR}/PfemFluidDynamicsApplication
+add_app ${KRATOS_APP_DIR}/DelaunayMeshingApplication
+add_app ${KRATOS_APP_DIR}/DEMApplication
+add_app ${KRATOS_APP_DIR}/SwimmingDEMApplication
+
+# Clean
+clear
+rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/cmake_install.cmake"
+rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/CMakeCache.txt"
+rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/CMakeFiles"
+
+# Configure
+cmake -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" \
+ -DUSE_METIS_5=OFF \
+ -DUSE_MPI=OFF \
+ -DADD_DEPENDENT_APPLICATIONS=ON \
+ -DINCLUDE_SUPERLU=ON \
+ -DINSTALL_EMBEDDED_PYTHON=ON \
+ -DKRATOS_BUILD_TESTING=OFF \
+
+# Buid
+cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target install -- -j6
