0c206747c970ed633432f0e6a1c75ffcbe40f54c
sunethwarna <suneth.warna@gmail.com>
2020-01-31 16:03:52 +0100

    updating epsilon wall condition

--- applications/RANSApplication/custom_conditions/evm_k_epsilon/rans_evm_k_epsilon_epsilon_wall.cpp
+++ applications/RANSApplication/custom_conditions/evm_k_epsilon/rans_evm_k_epsilon_epsilon_wall.cpp
@@ -11,8 +11,8 @@
 //
 
 // System includes
-#include <limits>
 #include <cmath>
+#include <limits>
 
 // External includes
 
@@ -123,7 +123,7 @@ void RansEvmKEpsilonEpsilonWall<TDim, TNumNodes>::CalculateLocalVelocityContribu
     rRightHandSideVector.clear();
     rDampingMatrix.clear();
 
-    if (this->Is(SLIP))
+    if (this->GetValue(PARENT_CONDITION_POINTER)->Is(SLIP))
     {
         this->AddLocalVelocityContribution(rDampingMatrix, rRightHandSideVector,
                                            rCurrentProcessInfo);
@@ -245,8 +245,14 @@ void RansEvmKEpsilonEpsilonWall<TDim, TNumNodes>::AddLocalVelocityContribution(
 {
     KRATOS_TRY
 
-    const GeometryType& r_geometry = this->GetGeometry();
+    const ConditionType& r_parent_condition = *(this->GetValue(PARENT_CONDITION_POINTER));
 
+    const double y_plus = r_parent_condition.GetValue(Y_WALL);
+    const double u_tau = r_parent_condition.GetValue(FRICTION_VELOCITY);
+    const double nu = r_parent_condition.GetValue(KINEMATIC_VISCOSITY);
+    const double wall_height = u_tau / (y_plus * nu);
+
+    const GeometryType& r_geometry = this->GetGeometry();
     // Get Shape function data
     const GeometryType::IntegrationPointsArrayType& integration_points =
         r_geometry.IntegrationPoints(GeometryData::GI_GAUSS_2);
@@ -254,14 +260,12 @@ void RansEvmKEpsilonEpsilonWall<TDim, TNumNodes>::AddLocalVelocityContribution(
     MatrixType shape_functions = r_geometry.ShapeFunctionsValues(GeometryData::GI_GAUSS_2);
 
     const double area = r_geometry.DomainSize();
-
     // CAUTION: "Jacobian" is 2.0*A for triangles but 0.5*A for lines
     double J = (TNumNodes == 2) ? 0.5 * area : 2.0 * area;
 
     const double epsilon_sigma =
         rCurrentProcessInfo[TURBULENT_ENERGY_DISSIPATION_RATE_SIGMA];
-    const double c_mu_25 = std::pow(rCurrentProcessInfo[TURBULENCE_RANS_C_MU], 0.25);
-    const double eps = std::numeric_limits<double>::epsilon();
+
     for (IndexType g = 0; g < num_gauss_points; ++g)
     {
         const Vector& gauss_shape_functions = row(shape_functions, g);
@@ -271,29 +275,19 @@ void RansEvmKEpsilonEpsilonWall<TDim, TNumNodes>::AddLocalVelocityContribution(
             r_geometry, KINEMATIC_VISCOSITY, gauss_shape_functions);
         const double nu_t = RansCalculationUtilities::EvaluateInPoint(
             r_geometry, TURBULENT_VISCOSITY, gauss_shape_functions);
-        const double tke = RansCalculationUtilities::EvaluateInPoint(
-            r_geometry, TURBULENT_KINETIC_ENERGY, gauss_shape_functions);
         const double epsilon = RansCalculationUtilities::EvaluateInPoint(
             r_geometry, TURBULENT_ENERGY_DISSIPATION_RATE, gauss_shape_functions);
-        const double y_plus = RansCalculationUtilities::EvaluateInPoint(
-            r_geometry, RANS_Y_PLUS, gauss_shape_functions);
 
-        const double u_tau = c_mu_25 * std::sqrt(std::max(tke, 0.0));
+        const double value = weight * (nu + nu_t / epsilon_sigma) * wall_height;
 
-        if (y_plus > eps)
+        for (IndexType a = 0; a < TNumNodes; ++a)
         {
-            const double value =
-                weight * (nu + nu_t / epsilon_sigma) * u_tau / (y_plus * nu);
-
-            for (IndexType a = 0; a < TNumNodes; ++a)
+            for (IndexType b = 0; b < TNumNodes; ++b)
             {
-                for (IndexType b = 0; b < TNumNodes; ++b)
-                {
-                    rDampingMatrix(a, b) -=
-                        gauss_shape_functions[a] * gauss_shape_functions[b] * value;
-                }
-                rRightHandSideVector[a] += value * gauss_shape_functions[a] * epsilon;
+                rDampingMatrix(a, b) -=
+                    gauss_shape_functions[a] * gauss_shape_functions[b] * value;
             }
+            rRightHandSideVector[a] += value * gauss_shape_functions[a] * epsilon;
         }
     }
 
