71017e76b3822b6ab694744a7502285bdad88935
Philipp Bucher <philipp.bucher@tum.de>
2020-02-03 18:51:00 +0100

    adding skiptest if empire is not available

--- applications/MappingApplication/tests/test_empire_mapper_wrapper.py
+++ applications/MappingApplication/tests/test_empire_mapper_wrapper.py
@@ -1,8 +1,11 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
 import KratosMultiphysics as KM
+import KratosMultiphysics.KratosUnittest as KratosUnittest
 import basic_mapper_tests
+import os
 
+@KratosUnittest.skipUnless("EMPIRE_API_LIBSO_ON_MACHINE" in os.environ, "Empire libs not found")
 class EmpireMortarMapper(basic_mapper_tests.BasicMapperTests):
     @classmethod
     def setUpClass(cls):
