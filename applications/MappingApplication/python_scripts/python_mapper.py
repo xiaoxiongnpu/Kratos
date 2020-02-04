ae56c4a8c6d3558f44a2f680c60ecc15caa6ca01
Philipp Bucher <philipp.bucher@tum.de>
2020-02-04 12:29:20 +0100

    make using supparts possible

--- applications/MappingApplication/python_scripts/python_mapper.py
+++ applications/MappingApplication/python_scripts/python_mapper.py
@@ -11,9 +11,18 @@ class PythonMapper(object):
     def __init__(self, model_part_origin, model_part_destination, mapper_settings):
         self.model_part_origin = model_part_origin
         self.model_part_destination = model_part_destination
-        self.mapper_settings = mapper_settings # Note: no validation done here, should be done in derived class
 
+        if mapper_settings.Has("interface_submodel_part_origin"):
+            self.model_part_origin = self.model_part_origin.GetSubModelPart(
+                mapper_settings["interface_submodel_part_origin"].GetString())
+
+        if mapper_settings.Has("interface_submodel_part_destination"):
+            self.model_part_destination = self.model_part_destination.GetSubModelPart(
+                mapper_settings["interface_submodel_part_destination"].GetString())
+
+        self.mapper_settings = mapper_settings
         self.mapper_settings.ValidateAndAssignDefaults(self._GetDefaultSettings())
+
         self.echo_level = self.mapper_settings["echo_level"].GetInt()
 
     def Map(self, variable_origin, variable_destination, mapper_flags=KM.Flags()):
