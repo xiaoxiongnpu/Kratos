33df20c2456aafaaadb2b67a4f2bf3bc872278cb
sunethwarna <suneth.warna@gmail.com>
2020-02-20 13:29:48 +0100

    bug fix

--- applications/RANSApplication/custom_utilities/rans_calculation_utilities.cpp
+++ applications/RANSApplication/custom_utilities/rans_calculation_utilities.cpp
@@ -393,7 +393,7 @@ bool IsWall(const ConditionType& rCondition)
         {
             const ConditionType& r_parent_condition =
                 *(rCondition.GetValue(PARENT_CONDITION_POINTER));
-            if (r_parent_condition.Is(SLIP) && rCondition.Is(STRUCTURE))
+            if (r_parent_condition.Is(SLIP) && r_parent_condition.Is(STRUCTURE))
             {
                 return true;
             }
